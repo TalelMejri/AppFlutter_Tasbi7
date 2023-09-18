b703b51e94616920b18f6a3a7e0192bc16888664
TalelMejri <talelmejri8@gmail.com>
2023-09-18 19:48:24 +0100

    groupeNote and trier

--- lib/Pages/MoreDetails.dart
+++ lib/Pages/MoreDetails.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:tasbi7/data/tabTasbi7.dart';
+import 'package:tasbi7/data/note.dart';
 class MoreDetails extends StatefulWidget{
   const MoreDetails({super.key});
 
@@ -12,11 +13,21 @@ class MoreDetails extends StatefulWidget{
 
 class _MoreDetails extends State<MoreDetails>{
 
+List<Note> groupeNote=[
+  Note(desc: "sayAllah"),
+  Note(desc: "sayAllah"),
+  Note(desc: "sayAllah"),
+  Note(desc: "sayAllah"),
+  Note(desc: "sayAllah"),
+  Note(desc: "sayAllah"),
+];
+
   @override
   Widget build(BuildContext context){
+    groupeNote.sort((a, b) => a.status == b.status ? 0 : a.status ? 1 : -1);
     return Center(
-      child: ListView.builder(itemCount: Adhkar.length,itemBuilder: (content,index){
-        final item=Adhkar[index];
+      child: ListView.builder(itemCount: groupeNote.length,itemBuilder: (content,index){
+        final item=groupeNote[index];
         String number=(Random.secure().nextInt(9999)).toString();
         return Dismissible(
              key:Key(number),
@@ -26,13 +37,14 @@ class _MoreDetails extends State<MoreDetails>{
              ),
              onDismissed: (direction){
               setState(() {
-                Adhkar.remove(item);
+                item.status=true;
               });
              },
              child:Card(
               elevation: 45,
               child: ListTile(
-                title: Text(item),
+                title: Text(item.notedesc),
+                subtitle: Text(item.status==true ? 'Seen' : 'Not Yet'),
               ),
              ));
       }),
